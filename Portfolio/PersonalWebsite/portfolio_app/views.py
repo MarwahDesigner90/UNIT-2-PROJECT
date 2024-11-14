3aa41e4a1ab8b3959307a068dec11567166c40d3
MarwahDesigner90 <marwaty666@gmail.com>
2024-11-14 13:37:14 +0300

    Finish building the skills page

--- Portfolio/PersonalWebsite/portfolio_app/views.py
+++ Portfolio/PersonalWebsite/portfolio_app/views.py
@@ -8,7 +8,12 @@ def home_view(request:HttpRequest):
     return render(request, "portfolio_app/home.html")
 
 def experience_view(request:HttpRequest):
-    return render(request, "portfolio_app/experience.html")
+    # portfolio_items = PortfolioItem.objects.all()
+    # context = {
+    #     'portfolio_item_1': portfolio_items[0] if len(portfolio_items) > 0 else None,
+    #     'portfolio_item_2': portfolio_items[1] if len(portfolio_items) > 1 else None,
+    # }
+    return render(request, "portfolio_app/experience.html" ,  {})
 
 def skills_view(request:HttpRequest):
     return render(request, "portfolio_app/skills.html")

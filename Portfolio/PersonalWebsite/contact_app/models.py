478264cae4bda5833cb20be513bff1daf253ccd1
MarwahDesigner90 <marwaty666@gmail.com>
2024-11-15 10:05:55 +0300

    Modify the contact model and add an inner class

--- Portfolio/PersonalWebsite/contact_app/models.py
+++ Portfolio/PersonalWebsite/contact_app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 class ContactSubmission(models.Model):
+    class Service(models.TextChoices):
+        WEB_DEVELOPMENT = 'web_development', 'Web Development'
+        SEO_OPTIMIZATION = 'seo_optimization', 'SEO Optimization'
+        WEB_MAINTENANCE = 'web_maintenance', 'Web Maintenance'
+        UI_UX_DESIGN = 'ui_ux_design', 'UI/UX Design'
+        CI_CD = 'ci_cd', 'CI/CD'
+        RESPONSIVE_DESIGN = 'responsive_design', 'Responsive Design'
+        INQUIRIES = 'inquiries', 'Inquiries'  
+
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+    service = models.CharField(max_length=50, choices=Service.choices)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Message from {self.name} at {self.submitted_at}"
+def __str__(self):
+    return f"Message from {self.name} at {self.submitted_at}"
